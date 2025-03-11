ae08264e21ff33f0cae1b1eb04129f0fd407a4c1
zhouzt21 <zhou-zt21@mails.tsinghua.edu.cn>
2025-03-11 14:09:15 +0800

    add eval for cano policy with sim2sim, using ldm server and diffusion cano policy

--- homebot_sapien/eval/eval_il_server.py
+++ homebot_sapien/eval/eval_il_server.py
@@ -5,18 +5,19 @@ import sapien.core as sapien
 import torch
 import imageio
 
-# from transforms3d.euler import euler2quat, quat2euler
-from transforms3d.quaternions import qmult, qconjugate, quat2mat, mat2quat
-from typing import List
-from homebot_sapien.utils.math import wrap_to_pi, euler2quat, quat2euler, mat2euler, get_pose_from_rot_pos
+import sys
+sys.path.append("/home/zhouzhiting/Projects")
 
-import json
+from transforms3d.quaternions import qmult, qconjugate, quat2mat, mat2quat
 import requests
 from datetime import datetime
 
-from homebot_sapien.env.pick_and_place_panda import PickAndPlaceEnv
-# from Projects.homebot.config import PANDA_DATA
-PANDA_DATA = "/home/zhouzhiting/panda_data"
+from homebot.homebot_sapien.env.pick_and_place_panda import PickAndPlaceEnv
+from homebot.homebot_sapien.utils.math import wrap_to_pi, euler2quat, quat2euler, mat2euler, get_pose_from_rot_pos
+
+import pickle
+from diffusion_policy.policy import DiffusionPolicy
+import torchvision.transforms as transforms
 
 def eval_imitation_with_goal():
     """"
@@ -227,34 +228,139 @@ def eval_imitation_with_goal():
         # pickle.dump(success_list, open(os.path.join(save_path, f"info.pkl"), "wb"))
 
 
+#######  init_policy ########
+device = "cuda" if torch.cuda.is_available() else "cpu"
+OBS_NORMALIZE_PARAMS = pickle.load(open(os.path.join("/home/zhouzhiting/Data/panda_data/cano_policy_pd_2/", "norm_stats_1.pkl"), "rb"))
+
+pose_gripper_mean = np.concatenate(
+    [
+        OBS_NORMALIZE_PARAMS[key]["mean"]
+        for key in ["pose", "gripper_width"]
+    ]
+)
+pose_gripper_scale = np.concatenate(
+    [
+        OBS_NORMALIZE_PARAMS[key]["scale"]
+        for key in ["pose", "gripper_width"]
+    ]
+)
+proprio_gripper_mean = np.concatenate(
+    [
+        OBS_NORMALIZE_PARAMS[key]["mean"]
+        for key in ["proprio_state", "gripper_width"]
+    ]
+)
+proprio_gripper_scale = np.concatenate(
+    [
+        OBS_NORMALIZE_PARAMS[key]["scale"]
+        for key in ["proprio_state", "gripper_width"]
+    ]
+)
+
+cameras = ['third']
+usages = ['obs']
+
+policy_config = {
+    'lr': 1e-5,
+    'num_images': len(cameras) * len(usages),
+    'action_dim': 10,
+    'observation_horizon': 1,
+    'action_horizon': 1,
+    'prediction_horizon': 20,
+
+    'global_obs_dim': 10,
+    'num_inference_timesteps': 10,
+    'ema_power': 0.75,
+    'vq': False,
+}
+
+ckpt_path = "/home/zhouzhiting/Data/panda_data/diffusion_policy_checkpoints/20250307_191102/policy_step_250000_seed_0.ckpt"
+
+policy = DiffusionPolicy(policy_config)
+policy.deserialize(torch.load(ckpt_path))
+policy.eval()
+policy.cuda()
+
+###############################
+
+
+def process_action(action):
+    """
+        unnormalize the action to the original scale, prepare for the env step.
+        input: action: (10,), float, np
+        output: action: (10,), float, np
+    """
+    action = action * np.expand_dims(pose_gripper_scale, axis=0) + np.expand_dims(pose_gripper_mean, axis=0)
+    return action
+
+def process_data(all_cam_images, proprio_state):
+    """
+        process the data for diffusion policy model. 
+        input:  all_cam_images: (M, h, w, c),  uint8, np (M is the number of cameras)
+                proprio_state: (10,), float, np
+        output: image_data: (B, c, h, w),  normalized in [0,1], float, np
+                qpos_data: (10)
+    """
+    # all_cam_images = np.stack(image_list, axis=0)   # already in shape (B, h, w, c)
+    image_data = torch.from_numpy(all_cam_images)
+    image_data = torch.einsum('k h w c -> k c h w', image_data)
+
+    try:
+        k, c, h, w = image_data.shape
+        transformations = [
+            # transforms.CenterCrop((int(h * 0.95), int(w * 0.95))),
+            transforms.RandomCrop((int(h * 0.95), int(w * 0.95))),
+            # transforms.Resize((240, 320), antialias=True),
+            transforms.Resize((224, 224), antialias=True),
+        ]
+        for transform in transformations:
+            image_data = transform(image_data)
+            # print(front.shape, goal.shape)
+    except Exception as e:
+        print(e)
+
+    image_data = image_data / 255.0
+
+    # qpos = np.array([0.], dtype=float)
+    proprio_state = np.array(proprio_state)
+    proprio_state = (
+                            proprio_state - proprio_gripper_mean
+                    ) / proprio_gripper_scale
+    qpos_data = torch.from_numpy(proprio_state).float()
+
+    return image_data, qpos_data
+
+
 def eval_imitation():
     """"
-    Evaluate the imitation policy. from pick_and_place_panda env.
+        Evaluate the cano policy with sim2sim, from pick_and_place_panda random env.
+        Comunicate with the ldm server.
+        loop: num_eval
+            loop: num_obj
+                loop: num_pred
+                    step action 10 steps for each predict
     """
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     env = PickAndPlaceEnv(
         use_gui=False,
         device=device,
-        # obs_keys=("wrist-rgb", "tcp_pose", "gripper_width"),
         obs_keys=(),
         domain_randomize=True,
-        canonical=True,
-        action_relative="none"
+        canonical= False, #True,
+        action_relative="none",
+        allow_dir=["fruit"]
     )
 
-    cameras = ["third", "wrist"]
+    cameras = ["third"] #, "wrist"]
     usage = ["obs"]
 
     stamp = datetime.now().strftime("%Y%m%d-%H%M%S")
 
     save_dir = os.path.join("tmp", stamp)
-    # save_dir = "try"
-    # num_seeds = 10000
+
     num_eval = 10
     os.makedirs(save_dir, exist_ok=True)
-
-    # cnt_list = []
 
     from tqdm import tqdm
 
@@ -268,7 +374,6 @@ def eval_imitation():
         random_state = np.random.RandomState(seed=seed)
 
         model_id_list = list(env.objs.keys())
-        # print(model_id_list)
         random_state.shuffle(model_id_list)
 
         for ep_id, model_id in enumerate(model_id_list):
@@ -283,7 +388,9 @@ def eval_imitation():
                 codec="h264",
             ) for cam in cameras}
 
-            for step in tqdm(range(500)):
+            # for step in tqdm(range(500)):
+            num_pred = 30
+            for step in tqdm(range(num_pred)):
                 obs = env.get_observation()
                 for cam in cameras:
                     imageio.imwrite(os.path.join(ep_path, f"obs-{cam}.jpg"), obs[f"{cam}-rgb"])
@@ -310,13 +417,20 @@ def eval_imitation():
                         np.array([obs["gripper_width"]]),
                     ]
                 )
-                data = {"proprio_state": json.dumps(proprio_state.tolist())}
 
-                response = requests.post("http://localhost:9977/diffusion", files=files, data=data)
-                response = response.json()
-                # print(response)
+                ##### for eval   
+                response = requests.post("http://localhost:9977/ldm", files=files)
+                response_data = response.json()
 
-                actions = np.array(response["action"])
+                all_cam_images = np.array(response_data["samples"])
+                print(all_cam_images.shape)
+                image_data, qpos_data = process_data(all_cam_images, proprio_state)
+                image_data, qpos_data = image_data.cuda().unsqueeze(0), qpos_data.cuda().unsqueeze(0)
+                print(image_data.shape, qpos_data.shape)
+
+                pred_actions = policy(qpos_data, image_data).squeeze().cpu()
+                actions = process_action(pred_actions)
+
                 converted_actions = []
 
                 for i in range(10):
@@ -336,7 +450,6 @@ def eval_imitation():
                     )
                     pose_action = np.concatenate(
                         [
-                            [0, 0],
                             init_to_desired_pose[:3, 3],
                             mat2euler(init_to_desired_pose[:3, :3]),
                             [gripper_width]
@@ -346,7 +459,13 @@ def eval_imitation():
 
                     _, _, _, _, info = env.step(pose_action)
 
+                    for cam in cameras:
+                        video_writer[cam].append_data(obs[f"{cam}-rgb"])
+
             for writer in video_writer.values():
                 writer.close()
 
-            # exit()
+
+if __name__ == "__main__":
+    eval_imitation()
+    # eval_imitation_with_goal()
